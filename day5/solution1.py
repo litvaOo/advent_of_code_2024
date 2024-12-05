12eb98a82ef584fd8eb8125db58110c0ef5fd6ef
Danylo Lytovchenko <danylo.lytovchenko@pm.me>
2024-12-05 06:21:18 +0100

    Add solution 1 day 5

---
+++ day5/solution1.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+
+
+lines = []
+with open("input.txt") as f:
+    lines = f.readlines()
+
+line_pointer = 0
+hashmap = defaultdict(lambda: [])
+while "|" in lines[line_pointer]:
+    tmp = lines[line_pointer].split("|")
+    hashmap[int(tmp[0])].append(int(tmp[1]))
+    line_pointer += 1
+line_pointer += 1
+
+sum = 0
+for line in lines[line_pointer:]:
+    print(hashmap)
+    line_processed = list(map(int, line.split(",")))
+    correct = True
+    for key in hashmap.keys():
+        if key not in line_processed:
+            continue
+        for i in line_processed:
+            if i == key:
+                break
+            if i in hashmap[key]:
+                print(f"{i}, {key}")
+                correct = False
+                break
+    if correct:
+        sum += line_processed[len(line_processed) // 2]
+print(sum)
