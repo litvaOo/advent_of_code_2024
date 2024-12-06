99e7c6db81a5247bf86b26f6cf25b4484a89114e
Danylo Lytovchenko <danylo.lytovchenko@pm.me>
2024-12-06 06:26:30 +0100

    Add solution 1 day 6

---
+++ day6/solution1.py
@@ -0,0 +1,55 @@
+lines = []
+with open("input.txt") as f:
+    lines = f.readlines()
+
+matrix = []
+start = [0, 0]
+for line_num, line in enumerate(lines):
+    matrix.append([])
+    for i, c in enumerate(line):
+        if c == "#":
+            matrix[-1].append(-1)
+        elif c == "^":
+            start = [line_num, i]
+        else:
+            matrix[-1].append(1)
+
+direction = [-1, 0]
+sum = 0
+position = start
+print(position)
+while True:
+    try:
+        if position[0] + direction[0] < 0 or position[0] + direction[0] >= len(
+            matrix[0]
+        ):
+            break
+        if position[1] + direction[1] < 0 or position[0] + direction[1] >= len(matrix):
+            break
+        if matrix[position[0] + direction[0]][position[1] + direction[1]] == -1:
+            if direction == [-1, 0]:
+                direction = [0, 1]
+            elif direction == [0, 1]:
+                direction = [1, 0]
+            elif direction == [1, 0]:
+                direction = [0, -1]
+            else:
+                direction = [-1, 0]
+        else:
+            sum += matrix[position[0] + direction[0]][position[1] + direction[1]]
+            position[0] += direction[0]
+            position[1] += direction[1]
+            matrix[position[0]][position[1]] = 0
+    except IndexError:
+        break
+for line in matrix:
+    string = ""
+    for c in line:
+        if c == -1:
+            string += "#"
+        elif c == 0:
+            string += "X"
+        else:
+            string += "."
+    print(string)
+print(sum)
